78a05310f82fc4e1b3f1310733e995a303d235a0
Patrick Delaney <pthomasdelaney@gmail.com>
2022-11-17 14:53:03 -0800

    chore: updating a number of items
    * github workflows
    * pre-commit plugins
    * tox framework
    * commitlint

--- script/semver.sh
+++ script/semver.sh
@@ -3,11 +3,11 @@
 ## Prepare repository for a versioned release.
 
 ## Release version passed from arguments.
-VER=$1
+VERSION=$1
 
 # Name and Git branch of the current project.
 PROJECT=$(git rev-parse --show-toplevel)
 LIBRARY=$(basename $PROJECT)
 
 ## Bump the __version__ of the python package.
-sed -i "/^__version__\ =/s/=.*/=\ \"$VER\"/" $PROJECT/$LIBRARY/__init__.py
+sed -i "/^__version__\ =/s/=.*/=\ \"$VERSION\"/" $PROJECT/$LIBRARY/__init__.py
